626b30bb2a43c3afc76848ac86972d919f78349f
maksimov.yd <maksimov.yd@gmail.com>
2023-07-14 05:00:26 +0300

    add file brain_games/cli.py and add his in main func in brain_games/scripts/brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+
+from brain_games import cli
 def print_hello_brain():
     print('Welcome to the Brain Games!')
 
 def main():
     print_hello_brain()
+    cli.welcome_user()
 
 if __name__ == '__main__':
     main()
