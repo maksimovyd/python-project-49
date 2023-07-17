3e77a5c353b7379a590fd08d236baab6ee60c7cd
maksimov.yd <maksimov.yd@gmail.com>
2023-07-18 01:17:50 +0300

    changes on f:brain_calc.py and brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,53 +1,13 @@
 #!/usr/bin/env python3
 
-from random import randint
-import prompt
+import brain_games.scripts.driver.engine
 import brain_games.cli
-
-
-def proverka_chetnosty(chislo):
-    if chislo % 2 == 0:
-        return True
-    else:
-        return False
-
-
-def vopros_otvet(random_num, otvet):
-    prav_otvet = random_num % 2
-    if otvet == 'yes' and prav_otvet == 0:
-        return True
-    elif otvet == 'no' and prav_otvet == 1:
-        return True
-    else:
-        return False
-
-
-def corr_otvet(random_num):
-    if proverka_chetnosty(random_num) is True:
-        return 'yes'
-    else:
-        return 'no'
 
 
 def main():
     name = brain_games.cli.welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-    i = 0
-    success = True
-    while i <= 2:
-        random_num = randint(1, 100)
-        print('Question: ' + str(random_num))
-        otvet = prompt.string('Your answer: ')
-        if vopros_otvet(random_num, otvet) is True:
-            i += 1
-            print('Correct')
-            continue
-        else:
-            print("'" + otvet + "' is wrong answer ;(."
-                  " Correct answer was '" + corr_otvet(random_num) + "'.")
-            print("Let's try again, " + name + "!")
-            success = False
-            break
+    success = brain_games.scripts.driver.engine.games_engine(name, 'brain_even')
     if success:
         print("Congratulations, " + name + "!")
 
