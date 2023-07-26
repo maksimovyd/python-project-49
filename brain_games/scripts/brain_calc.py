f5b117a0dd9e5cd7e73c80128d44d0bdcdbdf5b7
maksimov.yd <maksimov.yd@gmail.com>
2023-07-26 15:33:48 +0300

    refactoring brain_calc

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,6 +2,9 @@
 
 import brain_games.cli
 import brain_games.scripts.driver.engine
+from random import choice
+from random import randint
+import prompt
 
 
 def main():
@@ -10,6 +13,26 @@ def main():
     success = brain_games.scripts.driver.engine.games_engine(name, 'brain_calc')
     if success:
         print("Congratulations, " + name + "!")
+
+
+def play_brain_calc(NAME):
+    RANDOM_NUM_ONE, RANDOM_NUM_TWO = randint(1, 100), randint(1, 100)
+    OPER = choice(['+', '-', '*'])
+    real_answer = calc_rotvet(RANDOM_NUM_ONE, RANDOM_NUM_TWO, OPER)
+    print('Question: ' + str(RANDOM_NUM_ONE) + ' ' + str(OPER) + ' ' + str(RANDOM_NUM_TWO))
+    if brain_games.scripts.driver.engine.final_output(real_answer, NAME):
+        return True
+    else:
+        return False
+
+
+def calc_rotvet(RANDOM_NUM_ONE, RANDOM_NUM_TWO, OPER):
+    if OPER == '+':
+        return RANDOM_NUM_ONE + RANDOM_NUM_TWO
+    elif OPER == '-':
+        return RANDOM_NUM_ONE - RANDOM_NUM_TWO
+    else:
+        return RANDOM_NUM_ONE * RANDOM_NUM_TWO
 
 
 if __name__ == '__main__':
--- brain_games/scripts/driver/engine.py
+++ brain_games/scripts/driver/engine.py
@@ -2,7 +2,45 @@
 
 from random import choice
 from random import randint
+import brain_games.scripts
 import prompt
+
+
+def final_output(REAL_ANSWER, NAME):
+    USER_ANSWER = prompt.string('Your answer: ')
+    if verify_answer(USER_ANSWER, REAL_ANSWER) is False:
+        print("'" + str(USER_ANSWER) + "' is wrong answer ;(."
+              " Correct answer was '" + str(REAL_ANSWER) + "'.")
+        print("Let's try again, " + NAME + "!")
+        return False
+    else:
+        return True
+
+
+def games_engine(NAME, NAME_GAME):
+    games = {
+        'brain_even': brain_games.scripts.brain_even.play_brain_even,
+        'brain_calc': brain_games.scripts.brain_calc.play_brain_calc,
+        'brain_gcd': brain_games.scripts.brain_gcd.play_brain_gcd,
+        'brain_progression': brain_games.scripts.brain_progression.play_brain_progression,
+        'brain_prime': brain_games.scripts.brain_prime.play_brain_prime
+    }
+    play_game = games.get(NAME_GAME)
+    if not play_game:
+        return False
+    for _ in range(3):
+        if play_game(NAME):
+            print('Correct')
+        else:
+            return False
+    return True
+
+
+def verify_answer(USER_ANSWER, REAL_ANSWER):
+    if str(USER_ANSWER) == str(REAL_ANSWER):
+        return True
+    else:
+        return False
 
 
 def proverka_chetnosty(chislo):
@@ -29,25 +67,7 @@ def corr_otvet(random_num):
         return 'no'
 
 
-def proverka_answer(otvet_user, real_otvet):
-    if str(otvet_user) == str(real_otvet):
-        return True
-    else:
-        return False
 
-
-def generate_operations():
-    oper = ['+', '-', '*']
-    return choice(oper)
-
-
-def calc_rotvet(rand_n1, rand_n2, oper):
-    if oper == '+':
-        return rand_n1 + rand_n2
-    elif oper == '-':
-        return rand_n1 - rand_n2
-    else:
-        return rand_n1 * rand_n2
 
 
 def calc_nod(r_n1, r_n2):
@@ -75,23 +95,7 @@ def play_brain_even(name):
         return True
 
 
-def play_brain_calc(name):
-    rand_n1 = randint(1, 100)
-    rand_n2 = randint(1, 100)
-    oper = generate_operations()
-    real_otvet = calc_rotvet(rand_n1, rand_n2, oper)
-    print('Question: ' + str(rand_n1) + ' ' + str(oper) + ' ' + str(rand_n2))
-    otvet = prompt.string('Your answer: ')
-    otvet = int(otvet)
-    prov_corr = proverka_answer(otvet, real_otvet)
-    otvet_vprint = str(real_otvet)
-    if prov_corr is False:
-        print("'" + str(otvet) + "' is wrong answer ;(."
-              " Correct answer was '" + otvet_vprint + "'.")
-        print("Let's try again, " + name + "!")
-        return False
-    else:
-        return True
+
 
 
 def play_brain_gcd(name):
@@ -100,7 +104,7 @@ def play_brain_gcd(name):
     print('Question: ' + str(rand_n1) + ' ' + str(rand_n2))
     real_otvet = calc_nod(rand_n1, rand_n2)
     otvet = prompt.string('Your answer: ')
-    prov_corr = proverka_answer(otvet, real_otvet)
+    prov_corr = verify_answer(otvet, real_otvet)
     otvet_vprint = str(real_otvet)
     if prov_corr is False:
         print("'" + str(otvet) + "' is wrong answer ;(."
@@ -117,7 +121,7 @@ def play_brain_progression(name):
     shag = randint(2, 7)
     real_otvet = vivod_posled(q_chisl, num_otvet, shag)
     otvet = prompt.string('Your answer: ')
-    prov_corr = proverka_answer(otvet, real_otvet)
+    prov_corr = verify_answer(otvet, real_otvet)
     otvet_vprint = str(real_otvet)
     if prov_corr is False:
         print("'" + str(otvet) + "' is wrong answer ;(."
@@ -126,25 +130,6 @@ def play_brain_progression(name):
         return False
     else:
         return True
-
-
-def games_engine(name, name_game):
-    games = {
-        'brain_even': play_brain_even,
-        'brain_calc': play_brain_calc,
-        'brain_gcd': play_brain_gcd,
-        'brain_progression': play_brain_progression,
-        'brain_prime': play_brain_prime
-    }
-    play_game = games.get(name_game)
-    if not play_game:
-        return False
-    for _ in range(3):
-        if play_game(name):
-            print('Correct')
-        else:
-            return False
-    return True
 
 
 def play_brain_prime(name):
